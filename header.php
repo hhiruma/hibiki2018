3a280c12ce861f293ee4705870a5457dcbe8bb1d
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2018-08-11 17:41:03 +0900

    initial commit

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+		<script src='<? echo get_template_directory_uri();?>/js/external_resources/vue.js'></script>
+		<script src='<? echo get_template_directory_uri();?>/js/external_resources/jquery-3.2.1.min.js'></script>
+
+		<!-- bg switcher -->
+		<script src="<?php echo get_template_directory_uri();?>/js/external_resources/jquery.bgswitcher.js"></script>
+
+		<!-- anime.js -->
+		<script src="<?php echo get_template_directory_uri();?>/js/external_resources/anime.min.js"></script>
+
+		<!-- flex slider-->
+		<script src='<? echo get_template_directory_uri();?>/js/external_resources/jquery.flexslider.js'></script>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/external_resources/flexslider/flexslider.css">
+
+		<!-- hover.css -->
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/external_resources/hover.css"></script>
+
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<header>
+		</header>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
+
+<script src="<?php echo get_template_directory_uri();?>/js/main.js"></script>
